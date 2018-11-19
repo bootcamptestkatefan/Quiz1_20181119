240b17a013a926ad71517c9717353ed4f31a9cd6
bootcamptestkatefan <kate.fan@oocl.com>
2018-11-19 17:30:05 +0800

    Finished the 3 questions

--- main/main.js
+++ main/main.js
@@ -1,9 +1,39 @@
 // Write your cade below:
-function caclRemaindar() {};
+function caclRemaindar(a,b) {
 
-function caclSum() {};
+var c = a % b;
+return c;
 
-function caclSumInConditon() {};
+};
+
+function caclSum(numArray1) {
+var result = 0;
+var num;
+for (var i=0; i < numArray1.length; i++){
+result += numArray1[i];
+
+
+}
+return result;
+
+};
+
+
+function caclSumInConditon(numArray, a) {
+
+var result = 0;
+var num;
+for (var i=0; i < numArray.length; i++){
+if (numArray[i]<a)
+{
+result += numArray[i];
+}
+else{
+result += 0;
+}
+}
+return result;
+};
 
 module.exports = {
     caclRemaindar,
